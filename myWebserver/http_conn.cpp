af49c5a74a6e26240b2a0771ce7d48cc16237cee
girlsdontget341 <1030684663@qq.com>
2024-11-20 23:19:19 +0800

    处理http报文

---
+++ myWebserver/http_conn.cpp
@@ -0,0 +1,310 @@
+#include "http_conn.h"
+
+const char* doc_root = "/home/nowcoder/linux/myWebserver/resources";
+
+// 定义HTTP响应的一些状态信息
+const char* ok_200_title = "OK";
+const char* error_400_title = "Bad Request";
+const char* error_400_form = "Your request has bad syntax or is inherently impossible to satisfy.\n";
+const char* error_403_title = "Forbidden";
+const char* error_403_form = "You do not have permission to get file from this server.\n";
+const char* error_404_title = "Not Found";
+const char* error_404_form = "The requested file was not found on this server.\n";
+const char* error_500_title = "Internal Error";
+const char* error_500_form = "There was an unusual problem serving the requested file.\n";
+
+int setnoblocking(int fd){//设置非阻塞
+    int old = fcntl(fd, F_GETFL);
+    int newoption = old | O_NONBLOCK;
+    fcntl(fd, F_SETFL, newoption);
+    return newoption;
+}
+
+void addfd(int epollfd, int fd, bool one_shot){//将fd添加进epoll中
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN | EPOLLRDHUP;
+    if(one_shot){//也就是防止EPOLLONESHOT事件
+        // 防止同一个通信被不同的线程处理
+        event.events |= EPOLLONESHOT;
+    }
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnoblocking(fd);
+}
+
+void removefd(int epollfd, int fd){//从epoll实例中删除fd
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, 0);
+    close(fd);
+}
+
+// 修改文件描述符，重置socket上的EPOLLONESHOT事件，以确保下一次可读时，EPOLLIN事件能被触发
+void modfd(int epollfd, int fd, int ev) {
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = ev | EPOLLET | EPOLLONESHOT | EPOLLRDHUP;
+    epoll_ctl( epollfd, EPOLL_CTL_MOD, fd, &event );
+}
+
+// 所有的客户数
+int http_conn::m_user_count = 0;
+
+int http_conn::m_epollfd = -1;
+
+void http_conn::close_conn(){//关闭连接
+    if(m_sockfd != -1){
+        removefd(m_epollfd, m_sockfd);
+        m_sockfd = -1;
+        m_user_count--;
+    }
+}
+
+void http_conn::init(int sockfd, const sockaddr_in& addr){//初始化新连接
+    m_address = addr;
+    m_sockfd = sockfd;
+    // 端口复用
+    int reuse = 1;
+    setsockopt( m_sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof( reuse ) );
+    addfd( m_epollfd, sockfd, true );
+    m_user_count++;
+    init();
+}
+
+void http_conn::init()
+{
+
+    bytes_to_send = 0;
+    bytes_have_send = 0;
+
+    m_check_state = CHECK_STATE_REQUESTLINE;    // 初始状态为检查请求行
+    m_linger = false;       // 默认不保持链接  Connection : keep-alive保持连接
+
+    m_method = GET;         // 默认请求方式为GET
+    m_url = 0;              
+    m_version = 0;
+    m_content_length = 0;
+    m_host = 0;
+    m_start_line = 0;
+    m_checked_idx = 0;
+    m_read_idx = 0;
+    m_write_idx = 0;
+    //全部初始化为0
+    bzero(m_read_buf, READ_BUFFER_SIZE);
+    bzero(m_write_buf, READ_BUFFER_SIZE);
+    bzero(m_real_file, FILENAME_LEN);
+}
+
+// 循环读取客户数据，直到无数据可读或者对方关闭连接
+bool http_conn::read() {
+    if( m_read_idx >= READ_BUFFER_SIZE ) {
+        return false;
+    }
+    int bytes_read = 0;
+    while(true) {
+        // 从m_read_buf + m_read_idx索引出开始保存数据，大小是READ_BUFFER_SIZE - m_read_idx
+        bytes_read = recv(m_sockfd, m_read_buf + m_read_idx, 
+        READ_BUFFER_SIZE - m_read_idx, 0 );
+        if (bytes_read == -1) {
+            if( errno == EAGAIN || errno == EWOULDBLOCK ) {
+                // 没有数据
+                break;
+            }
+            return false;
+        }else if(bytes_read == 0){//关闭连接了
+            return false;
+        }
+        m_read_idx += bytes_read;
+    }
+    return true;
+}
+
+// 解析一行，判断依据\r\n
+http_conn::LINE_STATUS http_conn::parse_line(){
+    char temp;
+    for(;m_checked_idx < m_read_idx; m_checked_idx++){
+        temp = m_read_buf[m_checked_idx];
+        if(temp == '\r'){
+            if(m_checked_idx + 1 == m_read_idx){//缺少\n 还需要数据
+                return LINE_OPEN;
+            }else if(m_read_buf[m_checked_idx + 1] == '\n'){//把\r\n全部换成\0方便截取
+                m_read_buf[m_checked_idx++] = '\0';
+                m_read_buf[m_checked_idx++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }else if(temp == '\n'){//同理
+            if((m_checked_idx > 1) && (m_read_buf[m_checked_idx - 1] == '\r')){
+                m_read_buf[m_checked_idx - 1] = '\0';
+                m_read_buf[m_checked_idx++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }
+    }
+    return LINE_OPEN;
+}
+
+// 解析HTTP请求行，获得请求方法，目标URL,以及HTTP版本号
+http_conn::HTTP_CODE http_conn::parse_request_line(char* text){
+    m_url = strpbrk(text, " \t");//返回第一个空格或者制表符所对应的指针
+    if(!m_url){
+        return BAD_REQUEST;
+    }
+    *m_url++ = '\0';//空格改为结束符 作用是做到分割字符串
+    // GET\0/index.html HTTP/1.1 此时text是GET而m_url对应后面
+    char* method = text;
+    if(strcasecmp(method, "get") == 0){//不区分大小写比较字符串
+        m_method = GET;
+    }else{
+        return BAD_REQUEST;
+    }
+    m_version = strpbrk(m_url, " \t");//道理同上
+    if(!m_version){
+        return BAD_REQUEST;
+    }
+    *m_version++ = '\0';
+    if(strcasecmp(m_version, "HTTP/1.1") != 0){
+        return BAD_REQUEST;
+    }
+    //eg : http://192.168.110.129:10000/index.html
+    if(strncasecmp(m_url, "http://", 7) == 0){//比较前7个字符
+        m_url += 7;//跳过它们
+        m_url = strchr(m_url, '/');//返回后面第一个出现的/ 即/index.html
+    }
+    if(!m_url || (m_url[0] != '/')){
+        return BAD_REQUEST;
+    }
+    m_check_state = CHECK_STATE_HEADER; // 检查状态变成检查头
+    return NO_REQUEST;
+}
+
+// 解析HTTP请求的一个头部信息
+http_conn::HTTP_CODE http_conn::parse_headers(char* text){
+    // 遇到空行，表示头部字段解析完毕（符合http报文特点 最后以空行结尾）
+    if(text[0] == '\0'){
+        // 如果HTTP请求有消息体，则还需要读取m_content_length字节的消息体，
+        // 状态机转移到CHECK_STATE_CONTENT状态
+        if(m_content_length != 0){
+            m_check_state = CHECK_STATE_CONTENT;
+            return NO_REQUEST;
+        }
+        // 否则说明我们已经得到了一个完整的HTTP请求
+        return GET_REQUEST;
+    }else if(strncasecmp(text, "Connection:", 11) == 0){
+        // 处理Connection 头部字段  Connection: keep-alive
+        text += 11;
+        text += strspn(text, " \t");
+        if(strcasecmp(text, "keep-alive") == 0){
+            m_linger = true;
+        }
+    }else if(strncasecmp(text, "Content-Length:", 15) == 0){
+        // 处理Content-Length头部字段
+        text += 15;
+        text += strspn(text, " \t");
+        m_host = text;
+    }else if(strncasecmp( text, "Host:", 5 ) == 0 ){
+         // 处理Host头部字段
+        text += 5;
+        text += strspn( text, " \t" );
+        m_host = text;
+    }else{
+        printf( "oop! unknow header %s\n", text );
+    }
+    return NO_REQUEST;
+}
+
+// 我们没有真正解析HTTP请求的消息体，只是判断它是否被完整的读入了
+http_conn::HTTP_CODE http_conn::parse_content(char* text){
+    if(m_read_idx >= m_checked_idx + m_content_length){
+        text[m_content_length] = '\0';
+        return GET_REQUEST;
+    }
+    return NO_REQUEST;
+}
+
+// 主状态机，解析请求
+http_conn::HTTP_CODE http_conn::process_read(){
+    //初始化行状态和请求意义
+    LINE_STATUS line_status = LINE_OK;
+    HTTP_CODE ret = NO_REQUEST;
+    char* text = 0;
+    /*
+    m_check_state == CHECK_STATE_CONTENT：表示当前正在解析 HTTP 请求的消息体部分（即 Content 部分）。
+    在处理请求头后，如果消息体有内容，则需要继续解析请求体。
+    line_status == LINE_OK：表示当前读取的 HTTP 请求行格式是正确的，能够继续解析。
+    换句话说，它指示当前行的数据是有效的。
+    */
+    while((m_check_state == CHECK_STATE_CONTENT && line_status == LINE_OK) ||
+    (line_status = parse_line()) == LINE_OK){
+        text = get_line();//获取读缓冲区域数据
+        m_start_line = m_checked_idx;
+        printf( "got 1 http line: %s\n", text );
+
+        switch ( m_check_state ) {
+            case CHECK_STATE_REQUESTLINE: {
+                ret = parse_request_line( text );
+                if ( ret == BAD_REQUEST ) {
+                    return BAD_REQUEST;
+                }
+                break;
+            }
+            case CHECK_STATE_HEADER: {
+                ret = parse_headers( text );
+                if ( ret == BAD_REQUEST ) {
+                    return BAD_REQUEST;
+                } else if ( ret == GET_REQUEST ) {
+                    return do_request();
+                }
+                break;
+            }
+            case CHECK_STATE_CONTENT: {
+                ret = parse_content( text );
+                if ( ret == GET_REQUEST ) {
+                    return do_request();
+                }
+                line_status = LINE_OPEN;
+                break;
+            }
+            default: {
+                return INTERNAL_ERROR;
+            }
+        }
+    }  
+    return NO_REQUEST; 
+}
+
+// 当得到一个完整、正确的HTTP请求时，我们就分析目标文件的属性，
+// 如果目标文件存在、对所有用户可读，且不是目录，则使用mmap将其
+// 映射到内存地址m_file_address处，并告诉调用者获取文件成功
+http_conn::HTTP_CODE http_conn::do_request(){
+    strcpy(m_real_file, doc_root );//复制文件地址
+    int len = strlen(doc_root);
+    //拼接完整地址 "/home/nowcoder/webserver/resources"+"/index.html"
+    strncpy(m_real_file + len, m_url, FILENAME_LEN - len - 1);
+    // 获取m_real_file文件的相关的状态信息，-1失败，0成功
+    if(stat(m_real_file, &m_file_stat) < 0){
+        return BAD_REQUEST;
+    }
+    // 判断访问权限
+    if(!(m_file_stat.st_mode & S_IROTH)){
+        return FORBIDDEN_REQUEST;
+    }
+    // 判断是否是目录
+    if(S_ISDIR(m_file_stat.st_mode)){
+        return BAD_REQUEST;
+    }
+    // 以只读方式打开文件
+    int fd = open(m_real_file, O_RDONLY);
+    //内存映射
+    m_file_address = (char *)mmap(0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
+    return FILE_REQUEST;
+}
+
+//解除内存映射
+void http_conn::unmap(){
+    if(m_file_address){
+        munmap(m_file_address, m_file_stat.st_size);
+        m_file_address = 0;
+    }
+}
+
---
+++ myWebserver/main.cpp
@@ -0,0 +1,138 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include "locker.h"
+#include "threadpool.h"
+#include "http_conn.h"
+#include <ctype.h>
+#include <signal.h>
+#include <cassert>
+
+#define MAX_FD 65536 //最大文件描述符数量
+#define MAX_EVENT_NUMBER 10000 //监听的最大事件数量
+
+extern void addfd(int epollfd, int fd, bool one_shot);
+extern void removefd(int epollfd, int fd);
+
+int isValidPort(const char* port){//检验输入是否为有效端口号
+    for(int i = 0; port[i] != '\0'; i++)//?""
+    {
+        if(!isdigit(port[i])){
+            return 0;
+        }
+    }
+    int port_num = atoi(port);
+    return port_num > 1 && port_num < 65535;
+}
+
+void addsig(int sig, void(handler)(int)){//捕捉信号（函数指针）
+    struct sigaction sa;
+    memset(&sa, '\0', sizeof(sa));//初始化结构体
+    sa.sa_handler = handler;
+    sigfillset(&sa.sa_mask);
+    //屏蔽所有信号（在信号处理函数执行期间，所有其他信号都会被阻塞，防止信号嵌套（即同一信号重复触发或其他信号干扰处理过程））
+    assert( sigaction(sig, &sa, NULL) != -1);
+    //用于在运行时检查一个条件是否为真，如果条件不满足，则运行时将终止程序的执行并输出一条错误信息
+}
+
+int main(int argc, char* argv[]){//前面表示命令行参数个数 后面是字符串指针表示命令
+    if(argc <= 1){//说明只输入了1个参数 缺少端口号
+        printf("usage: %s port_number\n", basename(argv[0]));
+        return 1;
+    }
+    if(!isValidPort(argv[1])){//检验端口号是否合法
+        printf("Error: Invalid port number '%s'. Please provide a number between 1 and 65535.\n", argv[1]);
+        return 1;
+    }
+
+    int port = atoi(argv[1]);
+    addsig(SIGPIPE, SIG_IGN);//忽略SIGPIPE信号
+
+    threadpool<http_conn> * pool = NULL;
+    try{
+        pool = new threadpool<http_conn>;
+    }catch(...){//表示捕获所有异常
+        return 1;
+    }
+
+    http_conn* users = new http_conn[MAX_FD];//存储客户端的信息
+    int listenfd = socket(PF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in address;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+
+    int reuse = 1;//设置端口复用
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+    int ret = 0;
+    ret = bind(listenfd, (struct sockaddr*)&address, sizeof(address));
+    ret = listen(listenfd, 5);
+
+    epoll_event events[MAX_EVENT_NUMBER];
+    int epollfd = epoll_create(5);
+    addfd(epollfd, listenfd, false);
+    http_conn::m_epollfd = epollfd;
+
+    while(1){
+        //来等待并处理 epoll 实例中已就绪的事件
+        int num = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
+        if((num < 0) && (errno != EINTR)){
+            //errno != EINTR：EINTR 是一个错误代码，表示系统调用被信号中断。
+            //如果 errno 等于 EINTR，这通常意味着 epoll_wait() 被一个信号中断，
+            //可以忽略这个错误并重新调用 epoll_wait()。
+            printf("epoll failure\n");
+            break;
+        }
+
+        for(int i = 0; i<num; i++){
+            int sockfd = events[i].data.fd;
+            if(sockfd == listenfd){
+                struct sockaddr_in client_address;
+                socklen_t client_addrlength = sizeof(client_address);
+                int connfd = accept(listenfd, (struct sockaddr *)&client_address, &client_addrlength);
+                if(connfd < 0){
+                    printf( "errno is: %d\n", errno );
+                    continue;
+                }
+                if(http_conn::m_user_count > MAX_FD){//连接数超过限制
+                    close(connfd);
+                    continue;
+                }
+                users[connfd].init(sockfd, client_address);
+            }else if(events[i].events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR)){
+                /*EPOLLRDHUP：这个事件表示远程端已经关闭连接（如，TCP连接的对端发送了 FIN）。它通常意味着客户端关闭了连接。
+                EPOLLHUP：该事件表示连接被挂起，通常意味着远端连接被断开或连接出现了问题。
+                EPOLLERR：表示套接字发生错误。
+                这个条件判断用于处理以下情况：
+                如果发生了上述事件之一，表示客户端的连接已经出错或关闭。
+                在这种情况下，我们需要关闭连接。*/
+                users[sockfd].close_conn();
+            }else if(events[i].events & (EPOLLIN)){
+                if(users[sockfd].read()){
+                    pool->append(users + sockfd);
+                }else{
+                    users[sockfd].close_conn();
+                }
+            }else if(events[i].events & (EPOLLOUT)){
+                if(!users[sockfd].write()){
+                    users[sockfd].close_conn();
+                }
+            }
+        }
+    }
+    close(epollfd);
+    close(listenfd);
+    delete [] users;
+    delete pool;
+
+    return 0;
+}
